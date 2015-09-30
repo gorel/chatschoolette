ffe811437d4ee398eb4f24a97dc6578d0f7d34cf
Stephen Hong <mrsakkaro@pal-nat184-104-237.itap.purdue.edu>
2015-09-30 16:55:04 -0400

    auto pull modified

--- auto-pull.sh
+++ auto-pull.sh
@@ -8,6 +8,7 @@ if [ $LOCAL = $REMOTE ]; then
 	echo "Up-to-date"
 elif [ $LOCAL = $BASE ]; then
 	echo "Need to pull"
+	kill $(ps aux | grep '[S]CREEN -dmS cs408 python run.py' | awk '{print $2}'); git pull; screen -dmS cs408 python run.py
 elif [ $REMOTE = $BASE ]; then
 	echo "Need to push"
 else
